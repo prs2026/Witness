2da5f5316b7788931af0a1099ccdab7691b680f8
Polaris S <ejfsohn@Gmail.com>
2026-09-09 14:37:27 -0700

    versioned the code

--- Software/bloink/blink/main/blink_example_main.c
+++ Software/bloink/blink/main/blink_example_main.c
@@ -91,7 +91,7 @@ void app_main(void)
     configure_serial_echo();
 
     while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        ESP_LOGI(TAG, "Turning the LED %s! NEW PROGRAm", s_led_state == true ? "ON" : "OFF");
         blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
